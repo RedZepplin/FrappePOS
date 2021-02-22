71346aa6c99ad775dde36e9a2486fcbdd59d9d72
Juan Hanekom <juan@crossworx.co.za>
2021-02-22 14:36:27 +0200

    update project to latest react-native + refactor

---
+++ windows/FrappePOS/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::FrappePOS::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::FrappePOS::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
